d0c07ad653fee2472430d6a42aac000157f869de
maritzbuchholz <maritzbuchholz@gmail.com>
2025-12-06 22:14:10 -0500

    fix blinker linger

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 "use strict"
 
 const start = document.querySelector(".metalnome__start");
+const blinker = document.querySelector(".metalnome__blinker");
 
 
 // this function creates the visual time-keeper of a broken heart </3
 
 
 function blinkOn() {
-    const blinker = document.querySelector(".metalnome__blinker");
     blinker.classList.add("metalnome__blinker--on");
 }
 
 function blinkOff() {
-    const blinker = document.querySelector(".metalnome__blinker");
     blinker.classList.remove("metalnome__blinker--on");
 }
 
@@ -26,29 +25,37 @@ function basicStart () {
     const metroLoop = new Tone.Loop(
         (time) => {
             synth.triggerAttackRelease("G#4", "16n", time);
-            Tone.Draw.schedule(blinkOn, time);
+            Tone.Draw.schedule(()=> {
+                if(metalnomeOn) blinkOn();
+            }, time);
         },"8n"
     ).start(0);
     const metroLoop2 = new Tone.Loop(
         (time) => {
-            Tone.Draw.schedule(blinkOff, time);
+            Tone.Draw.schedule(()=> {
+                if(metalnomeOn) blinkOff();
+            }, time);
         },"8n"
     ).start("16n");
     Tone.Transport.start();
 }
 
 function stop() {
+    metalnomeOn = false;
     Tone.Transport.stop();
-    Tone.Transport.cancel(); // Clears all scheduled events
+    Tone.Transport.cancel();
+    Tone.Draw.cancel();
+    blinkOff();
 }
 
 
 let metalnomeOn = false;
 start.addEventListener("click", async (event) => {
     event.preventDefault();
+    const blinker = document.querySelector(".metalnome__blinker");
+    await Tone.start(); //connects to the WebAudio API and enable AudioContext
     metalnomeOn = !metalnomeOn;
     if (metalnomeOn){
-        await Tone.start();
         basicStart();
     } else {
         stop();
